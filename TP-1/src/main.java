fa2e5d73a2d8911045fc44b08c37914f1c6a9057
Bruno Moacir <brunopmoacir129@gmail.com>
2026-08-26 15:33:45 -0300

    att main

--- TP-1/src/main.java
+++ TP-1/src/main.java
@@ -22,9 +22,38 @@ public class main {
                 System.out.println("\nIniciando carga de dados...");
                 // Passa o caminho do CSV e onde o arquivo binário deve ser gerado
                 ImportadorCSV.processarArquivo("dados/imdb_movies.csv", "dados/dados.bin");
-            } else if (opcao == 0) {
+            } 
+            else if (opcao == 2) {
+                System.out.print("\nDigite o ID do filme que deseja ler: ");
+                int idBusca = scanner.nextInt();
+                
+                ArquivoBinario arqBin = new ArquivoBinario("dados/dados.bin");
+                Filme encontrado = arqBin.ler(idBusca); // Chama o método de leitura
+                
+                if (encontrado != null) {
+                    System.out.println("\n--- Filme Encontrado ---");
+                    System.out.println(encontrado.toString());
+                } else {
+                    System.out.println("\nFilme com ID " + idBusca + " não encontrado ou foi deletado.");
+                }
+            } 
+            else if (opcao == 4) {
+                System.out.print("\nDigite o ID do filme que deseja deletar: ");
+                int idDeleta = scanner.nextInt();
+                
+                ArquivoBinario arqBin = new ArquivoBinario("dados/dados.bin");
+                boolean sucesso = arqBin.deletar(idDeleta); // Chama o método de exclusão
+                
+                if (sucesso) {
+                    System.out.println("\nFilme deletado com sucesso!");
+                } else {
+                    System.out.println("\nFalha ao deletar: Filme não encontrado.");
+                }
+            }
+            else if (opcao == 0) {
                 System.out.println("Saindo do sistema...");
-            } else {
+            } 
+            else {
                 System.out.println("\nFuncionalidade em desenvolvimento para as próximas etapas.");
             }
             System.out.println();
